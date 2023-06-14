6e57ded6d5d29a9feb262985bc738d830651557c
Peter <2892084432j@gmail.com>
2023-06-14 19:01:56 -0400

    update

--- rcon.ts
+++ rcon.ts
@@ -2,26 +2,30 @@ import { getInstanceGame, isRconAvailable, sendRconCommand, waitTillRconAvailabl
 
 export type RconAcquisitionError = "Unsupported" | "Unavailable" | "Unknown";
 
-
+/**
+ * A class that represents a Rcon connection to a Minecraft Java instance.
+ * 
+ * This class is a ZST (Zero-Sized-Type), meaning that it has no fields and is only used for type checking and to prove, to some degree, that the Rcon connection is valid.
+ */
 export class Rcon {
     private constructor() { }
     /**
-     * Attempts to acquire an Rcon instance.
+     * Attempts to acquire a Rcon connection.
      * 
-     * @throws RconAcquisitionError if the Rcon instance could not be acquired.
+     * @throws RconAcquisitionError if the Rcon connection could not be acquired.
      * 
      * Some reasons for this error include:
      * 
-     * - Rcon server is not available.
+     * - Rcon connection is not available.
      * 
      * - The instance is not a Minecraft Java instance.
      * 
      * - The instance did not have rcon enabled in its server.properties file.
      * 
-     * @returns An Rcon instance if successful.
+     * @returns A Rcon instance if successful.
      * 
      */
-    static async acquire(): Promise<Rcon> {
+    static async tryAcquire(): Promise<Rcon> {
         const game = await getInstanceGame();
         if (game.type !== "MinecraftJava") {
             throw "Unsupported";
@@ -37,8 +41,10 @@ export class Rcon {
      * Acquires a Rcon instance, the promise will resolve when rcon is available.
      * 
      * @throws RconAcquisitionError if the underlying instance does not support rcon.
+     * 
+     * Note: This function will resolve only when rcon is available via busy polling.
      */
-    static async waitForAcquisition(): Promise<Rcon> {
+    static async acquire(): Promise<Rcon> {
         const game = await getInstanceGame();
         if (game.type !== "MinecraftJava") {
             throw "Unsupported";
